06c2124e851830e8e7115e116260432ba48f6568
Rob Hird <robertjhird@gmail.com>
2022-05-14 16:49:19 -0400

    section 3 just finished children props lesson

--- Section_3_React_Basics/01-starting-setup/src/components/ExpenseItem.js
+++ Section_3_React_Basics/01-starting-setup/src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 // A component in react is just a Javascript function
 import "./ExpenseItem.css";
-
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{props.title}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.date}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
